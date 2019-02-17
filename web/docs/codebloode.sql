e871a6166d2f09b170825fcca04278bbe9bf2115
silentwolf0 <cetokola@outlook.com>
2019-02-17 16:29:47 +0300

    new database

--- web/docs/codebloode.sql
+++ web/docs/codebloode.sql
@@ -1,112 +1,174 @@
--- MySQL dump 10.16  Distrib 10.1.29-MariaDB, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: codebloode
--- ------------------------------------------------------
--- Server version	10.1.29-MariaDB-6
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2019 at 02:26 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `counsellor_auth`
+-- Database: `codebloode`
 --
 
-DROP TABLE IF EXISTS `counsellor_auth`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `counsellor_auth` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `f_name` varchar(255) NOT NULL,
-  `l_name` varchar(255) NOT NULL,
-  `u_name` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL,
-  `salt` varchar(255) NOT NULL,
-  `day` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `counsellor_auth`
+-- Table structure for table `appointment`
 --
 
-LOCK TABLES `counsellor_auth` WRITE;
-/*!40000 ALTER TABLE `counsellor_auth` DISABLE KEYS */;
-/*!40000 ALTER TABLE `counsellor_auth` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `appointment` (
+  `appId` int(4) UNSIGNED NOT NULL,
+  `date` date DEFAULT NULL,
+  `time` time(6) DEFAULT NULL,
+  `counsNo` varchar(12) DEFAULT NULL,
+  `regNo` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Table structure for table `dean_auth`
+-- Dumping data for table `appointment`
 --
 
-DROP TABLE IF EXISTS `dean_auth`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `dean_auth` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `f_name` varchar(255) NOT NULL,
-  `l_name` varchar(255) NOT NULL,
-  `u_name` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL,
-  `salt` varchar(255) NOT NULL,
-  `day` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO `appointment` (`appId`, `date`, `time`, `counsNo`, `regNo`) VALUES
+(0, '0000-00-00', '09:15:00.000000', '1', NULL);
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `dean_auth`
+-- Table structure for table `counsellor`
 --
 
-LOCK TABLES `dean_auth` WRITE;
-/*!40000 ALTER TABLE `dean_auth` DISABLE KEYS */;
-/*!40000 ALTER TABLE `dean_auth` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `counsellor` (
+  `counsNo` varchar(12) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phoneNo` varchar(13) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `salt` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `student_auth`
+-- Table structure for table `dean`
 --
 
-DROP TABLE IF EXISTS `student_auth`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `student_auth` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `f_name` varchar(255) NOT NULL,
-  `l_name` varchar(255) NOT NULL,
-  `u_name` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL,
-  `salt` varchar(255) NOT NULL,
-  `day` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `dean` (
+  `fName` varchar(20) NOT NULL,
+  `lName` varchar(20) NOT NULL,
+  `uName` varchar(20) NOT NULL,
+  `password` varchar(75) NOT NULL,
+  `salt` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `student_auth`
+-- Table structure for table `schedule`
 --
 
-LOCK TABLES `student_auth` WRITE;
-/*!40000 ALTER TABLE `student_auth` DISABLE KEYS */;
-INSERT INTO `student_auth` VALUES (1,'Elvis','Mutende','SP13/00820/15','foYaWQggcZ+/xx7c5R8ru/26Ksp1q4eTUPyogzGRKVQ=','GPMFwIXG6bWGPr24h25l28ZAVDwvxe','2019-01-18 10:39:11');
-/*!40000 ALTER TABLE `student_auth` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE `schedule` (
+  `couns_Id` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Dumping data for table `schedule`
+--
+
+INSERT INTO `schedule` (`couns_Id`, `status`, `date`, `time`) VALUES
+(2, 'available', '2019-02-02', '09:00:00'),
+(2, 'available', '2019-02-02', '09:00:00'),
+(3, 'available', '2019-02-02', '09:00:00'),
+(3, 'available', '2019-02-02', '09:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scheduling`
+--
+
+CREATE TABLE `scheduling` (
+  `scheduleId` int(4) NOT NULL,
+  `counsNo` varchar(12) NOT NULL,
+  `dateAway` date NOT NULL,
+  `timeAway` time(6) NOT NULL,
+  `dateBack` date NOT NULL,
+  `timeBack` time(6) NOT NULL,
+  `reason` varchar(150) NOT NULL,
+  `approval` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `regNo` varchar(15) NOT NULL,
+  `fName` varchar(20) NOT NULL,
+  `lName` varchar(20) NOT NULL,
+  `phoneNo` varchar(13) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(75) NOT NULL,
+  `salt` varchar(75) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appId`),
+  ADD KEY `foreign key` (`regNo`);
+
+--
+-- Indexes for table `counsellor`
+--
+ALTER TABLE `counsellor`
+  ADD PRIMARY KEY (`counsNo`);
+
+--
+-- Indexes for table `scheduling`
+--
+ALTER TABLE `scheduling`
+  ADD PRIMARY KEY (`scheduleId`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`regNo`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `foreign key` FOREIGN KEY (`regNo`) REFERENCES `student` (`regNo`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-01-22 10:45:35
